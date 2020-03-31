190b0b6b790fcf4b3d6d7f88255bf17178aa6b39
MaksumNechiporuk <maksum.nec@gmail.com>
2020-03-31 14:03:16 +0200

    add api

---
+++ CarSale/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarSale.Entities;
+using CarSale.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CarSale
+{
+    public class Startup
+    {
+        private IConfigurationRoot _dbConf;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment ENV)
+        {
+            _dbConf = new ConfigurationBuilder().SetBasePath(ENV.ContentRootPath).AddJsonFile("settings.json").Build();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<DBContext>(options => options.UseSqlServer(_dbConf.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 2;
+            })
+               .AddEntityFrameworkStores<DBContext>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(
+              builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                DBContext content = scope.ServiceProvider.GetRequiredService<DBContext>();
+                DBObject.Initial(content);
+            }
+            app.UseMvc();
+        }
+    }
+}
