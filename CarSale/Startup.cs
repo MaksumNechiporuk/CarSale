df9075ce8a405346c013ef3b3c849684fa73c527
MaksumNechiporuk <maksum.nec@gmail.com>
2020-05-14 11:19:17 +0200

    docker

--- CarSale/Startup.cs
+++ CarSale/Startup.cs
@@ -72,7 +72,7 @@ namespace CarSale
                 {
                     spa.UseReactDevelopmentServer(npmScript: "start");
 
-
+                    //spa.UseReactDevelopmentServer(npmScript: "start --host 0.0.0.0:3000");
 
                     // spa.UseReactDevelopmentServer(npmScript: "start --host 0.0.0.0:3000");
 
